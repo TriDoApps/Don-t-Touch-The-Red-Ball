afd9022c4c41ef9a784f9bbb54412b5b39eb1d96
Tri Do <trido2@illinois.edu>
2018-07-07 18:17:15 -0500

    DontTouchTheRedBall created, and made the button "Play" interactable.

---
+++ app/src/main/java/com/example/donttouchtheredball/DontTouchTheRedBall.java
@@ -0,0 +1,13 @@
+package com.example.donttouchtheredball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DontTouchTheRedBall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dont_touch_the_red_ball);
+    }
+}
--- app/src/main/java/com/example/donttouchtheredball/TitleScreen.java
+++ app/src/main/java/com/example/donttouchtheredball/TitleScreen.java
@@ -1,13 +1,42 @@
 package com.example.donttouchtheredball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * Title screen for Don't Touch The Red Ball.
+ */
 public class TitleScreen extends AppCompatActivity {
 
+    private static final String TAG = "DTTRB:Title";
+
+    /**
+     * Run when this activity comes to the foreground.
+     *
+     * @param savedInstanceState unused
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        Log.d(TAG, "Opened title screen");
+
+        final Button play = findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked Play");
+                Intent intent = new Intent(TitleScreen.this, DontTouchTheRedBall.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
